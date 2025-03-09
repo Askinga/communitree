47ede3fb56e7ddf813f4566541cdfd9fb44e138a
Askinga <musicgemca@gmail.com>
2025-03-09 14:37:16 -0400

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -4,21 +4,21 @@ addLayer("f", {
     position: 0, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
     startData() { return {
         unlocked: true,
-		points: new ExpantaNum(0),
+		points: EN(0),
     }},
     color: "#F4ACA1",
-    requires: new ExpantaNum(10), // Can be a function that takes requirement increases into account
+    requires: EN(10), // Can be a function that takes requirement increases into account
     resource: "Flowers", // Name of prestige currency
     baseResource: "points", // Name of resource prestige is based on
     baseAmount() {return player.points}, // Get the current amount of baseResource
     type: "normal", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
     exponent: 0.5, // Prestige currency exponent
     gainMult() { // Calculate the multiplier for main currency from bonuses
-        mult = new ExpantaNum(1)
+        mult = EN(1)
         return mult
     },
     gainExp() { // Calculate the exponent on main currency from bonuses
-        return new ExpantaNum(1)
+        return EN(1)
     },
     row: 0, // Row the layer is in on the tree (0 is the first row)
     hotkeys: [
@@ -29,7 +29,7 @@ addLayer("f", {
 	11: {
 		title: "More Bees",
 		description: "x2 Bee gain.",
-		cost: new ExpantaNum(1)
+		cost: EN(1)
 	},
     },		
 })
