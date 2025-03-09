0015bd7151163fb4ea6245a7a5c6c399b1a50c51
Askinga <musicgemca@gmail.com>
2025-03-09 13:33:57 -0400

    Add files via upload

---
+++ js/layers/layers (1).js
@@ -0,0 +1,35 @@
+addLayer("f", {
+    name: "Flowers", // This is optional, only used in a few places, If absent it just uses the layer id.
+    symbol: "F", // This appears on the layer's node. Default is the id with the first letter capitalized
+    position: 0, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
+    startData() { return {
+        unlocked: true,
+		points: new ExpantaNum(0),
+    }},
+    color: "#F4ACA1",
+    requires: new ExpantaNum(10), // Can be a function that takes requirement increases into account
+    resource: "Flowers", // Name of prestige currency
+    baseResource: "points", // Name of resource prestige is based on
+    baseAmount() {return player.points}, // Get the current amount of baseResource
+    type: "normal", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
+    exponent: 0.5, // Prestige currency exponent
+    gainMult() { // Calculate the multiplier for main currency from bonuses
+        mult = new ExpantaNum(1)
+        return mult
+    },
+    gainExp() { // Calculate the exponent on main currency from bonuses
+        return new ExpantaNum(1)
+    },
+    row: 0, // Row the layer is in on the tree (0 is the first row)
+    hotkeys: [
+        {key: "f", description: "F: Reset for flowers", onPress(){if (canReset(this.layer)) doReset(this.layer)}},
+    ],
+    layerShown(){return true},
+    upgrades: {
+	11: {
+		title: "More Bees",
+		description: "x2 Bee gain.",
+		cost: new ExpantaNum(1)
+	},
+    },		
+})
